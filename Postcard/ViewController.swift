3808c7aae6589599348c5e1c6fad0569bc3d857d
Chris <gitsum@hiroic.cc>
2014-10-10 23:48:58 -0300

    first ver of project

---
+++ Postcard/ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  Postcard
+//
+//  Created by Chris on 9/30/14.
+//  Copyright (c) 2014 HiroicBeta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var nameField: UITextField!
+    @IBOutlet weak var messageField: UITextField!
+    @IBOutlet weak var mailButton: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func sendMail(sender: UIButton) {
+        messageLabel.hidden = false;
+        messageLabel.text = messageField.text;
+        messageField.text = "";
+        messageField.resignFirstResponder();
+        mailButton.setTitle("Sent!", forState: UIControlState.Normal);
+    }
+
+
+}
+
